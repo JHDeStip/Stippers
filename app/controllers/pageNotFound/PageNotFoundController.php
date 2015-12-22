319a5119ad384bc61c5e88384627f04709d1cbd7
Stannieman <st.stanie@hotmail.com>
2015-12-22 12:32:25 +0100

    Updated page not found controller to also handle post requests.

--- app/controllers/pageNotFound/PageNotFoundController.php
+++ app/controllers/pageNotFound/PageNotFoundController.php
@@ -1,17 +1,21 @@
 <?php
 
-require_once __DIR__."/../IController.php";
-require_once __DIR__."/../../helperClasses/View.php";
+require_once __DIR__.'/../IController.php';
+require_once __DIR__.'/../../helperClasses/Page.php';
 
-abstract class PageNotFoundController implements IController
-{
-    public static function get()
-    {
-        $data["title"] = "Pagina niet gevonden";
-        View::showBasicView(["pageNotFound/PageNotFoundView"], $data);
+abstract class PageNotFoundController implements IController {
+    public static function get() {
+        PageNotFoundController::buildPage();
     }
     
-    public static function post()
-    {
+    public static function post() {
+        PageNotFoundController::buildPage();
+    }
+    
+    private static function buildPage() {
+        $page = new Page();
+        $page->data['title'] = 'Pagina niet gevonden';
+        $page->addView('pageNotFound/PageNotFoundView');
+        $page->showBasic();
     }
 }

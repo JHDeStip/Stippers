88258bc9686a51029d0dc6ce441c9eafac0c752b
Stannieman <st.stanie@hotmail.com>
2016-02-03 16:05:58 +0100

    For every view a js file is loaded if it exists. Also added the ability to add extra javascript files.

--- app/helperClasses/Page.php
+++ app/helperClasses/Page.php
@@ -12,15 +12,21 @@
 require_once 'menu/MenuBuilder.php';
 
 class Page {
-    public $views = array();
-    public $data = null;
+    private $views = array();
+    public $data;
+    
+    function __construct() {
+       $this->data['viewJsFiles'] = array();
+       $this->data['extraJsFiles'] = array();
+    }
+
         
     /**
      * Shows a page with the views that have been previously given.
      * Views will be displayed in the order they were added.
      */
     public function showBasic() {
-        require_once __DIR__.'/../views/common/Header.html' ;
+        require_once __DIR__.'/../views/common/Header.html';
         foreach ($this->views as $view)
             require_once __DIR__.'/../views/'.$view.'.html';
         require_once __DIR__.'/../views/common/Footer.html';
@@ -45,6 +51,20 @@ class Page {
      * @param string $viewName name of the view to add
      */
     public function addView($viewName) {
+        //Add view to list
         array_push($this->views, $viewName);
+        //Add javascript file to list
+        if (file_exists(__DIR__.'/../js/views/'.$viewName.'.js'))
+                array_push($this->data['viewJsFiles'], $viewName);
+    }
+    
+    /**
+     * Adds a javascript file to the page.
+     * 
+     * @param string $jsFile name of the javascript file relative to the /app/js directory.
+     */
+    public function addExtraJsFile($jsFile) {
+        if (file_exists(__DIR__.'/../js/'.$jsFile))
+            array_push($this->data['extraJsFiles'], $jsFile);
     }
 }

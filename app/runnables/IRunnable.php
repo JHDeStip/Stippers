6aff8d1fd33e3629e37e634b4dd69411b6f9bd1e
Stannieman <st.stanie@hotmail.com>
2016-02-29 20:56:05 +0100

    Add runnable interface to enable creating runnables.

---
+++ app/runnables/IRunnable.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of the Stippers project (available here: https://github.com/Stannieman/stippers/).
+ * The license and all terms en conditions that apply to Stippers also apply to this file.
+ * 
+ * @author Stan Wijckmans
+ * 
+ * Interface for runnables.
+ */
+
+interface IController
+{
+    public static function run();
+}

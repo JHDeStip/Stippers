bc89d4b744101ae059f7c63b1dcbd074e4bcf138
Stan Wijckmans <stanw@aviobook.aero>
2022-01-30 13:30:25 +0100

    Clean up cronjob runner.

--- cronjobrunner.php
+++ cronjobrunner.php
@@ -1,23 +1,10 @@
 <?php
 
-/**
- * Location: /application/controllers
- * If the webhost allows only 1 cronjob this controller can be used to call all methods that should be run by a cronjob.
- */
+require_once __DIR__.'/../members/app/config/GlobalConfig.php';
+require_once __DIR__.'/../members/app/runnables/balanceSummaryEmailSender/BalanceSummaryEmailSender.php';
+require_once __DIR__.'/../members/app/runnables/weeklyWinnerPicker/WeeklyWinnerPicker.php';
+require_once __DIR__.'/../members/app/runnables/monthlyWinnerEmailSender/MonthlyWinnerEmailSender.php';
 
-if (!defined('BASEPATH'))
-    echo('No direct script access allowed');
-
-require_once 'import.php';
-
-require_once __DIR__.'/../../members/app/config/GlobalConfig.php';
-require_once __DIR__.'/../../members/app/runnables/balanceSummaryEmailSender/BalanceSummaryEmailSender.php';
-require_once __DIR__.'/../../members/app/runnables/weeklyWinnerPicker/WeeklyWinnerPicker.php';
-require_once __DIR__.'/../../members/app/runnables/monthlyWinnerEmailSender/MonthlyWinnerEmailSender.php';
-
-//Job to refresh the calendar on the website with Facebook events, this can happen at every cron run
-(new Import())->index();
-        
 //Get current time
 $dateTime = new DateTime();
 $dateTime->setTimeZone(new DateTimeZone(GlobalConfig::PHP_TIME_ZONE));
@@ -25,7 +12,7 @@ $dateTime->setTimeZone(new DateTimeZone(GlobalConfig::PHP_TIME_ZONE));
 //Jobs to do weekly things every monday between 7 and 8 a.m.
 if ($dateTime->format('w') == 1 && $dateTime->format('H') == 7) {
     //Send money transactions report email
-    BalanceSummaryEmailSender::run();
+    //BalanceSummaryEmailSender::run();
     //Generate winner of the week
     WeeklyWinnerPicker::run();
 }
@@ -35,3 +22,5 @@ if ($dateTime->format('j') == 1 && $dateTime->format('H') == 7) {
     //Send monthly winner notification
     MonthlyWinnerEmailSender::run();
 }
+
+?>
